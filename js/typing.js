590ffac250138c1ad483baf896e9fd64940124ed
Mirza Shakil <mirzashakil.13jan@gmail.com>
2025-10-03 10:13:31 +0600

    rbac, user creation

---
+++ js/typing.js
@@ -0,0 +1,21 @@
+var typing = new Typed('#typing', {
+    strings: ['General Manager of Amirath Lube', 'Oil Savant | Expert in Lubricant and Condition Monitoring'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    fadeOut: true,
+    loop: true,
+    backDelay: 2000,
+
+});
+
+// var typing = new Typed('#typing', {
+//     strings: [
+//         'General Manager of Amirath Lube',
+//         'Oil Savant | Expert in Lubricant and Condition Monitoring'
+//     ],
+//     typeSpeed: 50,
+//     backSpeed: 30,   // backspacing speed
+//     loop: true,
+//     backDelay: 2000, // wait before deleting
+//     // fadeOut: true, // চাইলে এই লাইন বাদ দিন
+// });
